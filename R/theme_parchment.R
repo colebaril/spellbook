0b12ae0af0c6de2795df0db75a75d880d8534b35
Cole Baril <cole@colebaril.ca>
2025-09-17 23:58:19 -0500

    Initial commit

---
+++ R/theme_parchment.R
@@ -0,0 +1,24 @@
+#' Theme Parchment
+#'
+#' A ggplot2 theme styled like old parchment and ink.
+#'
+#' @param p A ggplot object
+#' @return A themed ggplot object
+#' @export
+
+theme_parchment <- function(p) {
+  p +
+    theme_bw(base_size = 12,
+                 base_family = "Courier New",
+                 paper = "cornsilk", ink = "black") +
+    theme(
+      plot.title = element_text(face = "bold", size = 25, hjust = 0.5),
+      plot.subtitle = element_text(size = 15, hjust = 0.5),
+      plot.caption = element_text(size = 12),
+      legend.position = "top",
+      legend.key.width = unit(3, "lines"),
+      legend.title = element_text(face = "bold", size = 15, hjust = 0.5),
+      legend.title.position = "top",
+      legend.text = element_text(size = 12)
+    )
+}
