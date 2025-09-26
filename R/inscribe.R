c9966df8e7024f63730510c65b91296958a32589
Cole Baril <cole@colebaril.ca>
2025-09-25 19:07:50 -0500

    inscribe

--- R/inscribe.R
+++ R/inscribe.R
@@ -27,36 +27,26 @@ inscribe <- function(type = c("plot", "table"),
   
   github_icon <- "\uf09b"  
   bluesky_icon <- "\ue671"
-  
   social_caption <- glue::glue(
-    "<div style='text-align:left; display:inline-block;'>
-      <span style='font-weight:bold; color:#4d4d4d;'>Graphic:</span>
-      <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{github_icon}</span>
-      <span style='color:#4d4d4d;'>{github_username}</span>
-      <span style='margin-left:15px;'></span>
-      <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{bluesky_icon}</span>
-      <span style='color:#4d4d4d;'>{bluesky_username}</span>
-    </div>"
+    "<span style='font-weight:bold; color:#4d4d4d; '>Graphic:</span>
+  <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{github_icon}</span>
+   <span style='color:#4d4d4d;'>{github_username}</span>
+   <span style='margin-left:15px;'></span>
+   <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{bluesky_icon}</span>
+   <span style='color:#4d4d4d;'>{bluesky_username}</span>"
   )
   
   if (include_data_source && !is.null(data_source)) {
-    data_caption <- glue::glue(
-      "<div style='text-align:right; float:right; display:inline-block;'>
-        <span style='font-weight:bold; color:#4d4d4d;'>Data:</span> 
-        <span style='color:#4d4d4d;'>{data_source}</span>
-      </div>"
-    )
-    full_caption <- glue::glue("{social_caption}{data_caption}")
-  } else {
-    full_caption <- social_caption
+    social_caption <- glue::glue("{social_caption}  
+    <span style='font-weight:bold; color:#4d4d4d;'>Data:</span> <span style='color:#4d4d4d;'>{data_source}</span>")
   }
   
   if (type == "plot") {
     return(list(
-      labs(caption = full_caption),
-      theme(plot.caption = ggtext::element_textbox_simple(size = 12, face = "bold", halign = 0)) # 0 = left alignment, but HTML floats handle placement
+      labs(caption = social_caption),
+      theme(plot.caption = ggtext::element_textbox_simple(size = 12, face = "bold"))
     ))
   } else if (type == "table") {
-    return(function(x) gt::tab_source_note(x, gt::md(full_caption)))
+    return(function(x) gt::tab_source_note(x, gt::md(social_caption)))
   }
 }
